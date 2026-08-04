a4460aa349fa8556572d05ebdacd5c6b05128c5e
mdazad9236-dotcom <mdazad9236@gmail.com>
2026-08-04 13:57:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+os.environ['PYTHONUNBUFFERED'] = '1'
+
 from flask import Flask, render_template_string, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
