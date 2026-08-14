98679ef53416df6fcb625449147b75c7c4899504
mdazad9236-dotcom <mdazad9236@gmail.com>
2026-08-14 13:30:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, render_template
+
 from stock_service import StockService
+
 
 app = Flask(__name__)
 
@@ -8,14 +10,17 @@ service = StockService()
 
 @app.route("/")
 def home():
+
     return render_template("index.html")
 
 
 @app.route("/api/analyze/<symbol>", methods=["GET"])
 def analyze_endpoint(symbol):
+
     result = service.get_stock_analysis(symbol)
 
     if result.get("success"):
+
         return jsonify({
             "status": "success",
             "data": result
@@ -25,22 +30,14 @@ def analyze_endpoint(symbol):
         "status": "failed",
         "message": result.get(
             "message",
-            "Error fetching stock data"
+            "Unable to fetch stock data."
         )
     }), 400
 
 
-@app.route("/health", methods=["GET"])
-def health():
-    return jsonify({
-        "status": "running",
-        "message": "Stock Analysis App API is healthy"
-    })
-
-
 if __name__ == "__main__":
+
     app.run(
         host="0.0.0.0",
-        port=5000,
-        debug=False
+        port=5000
     )
